9b00bb1157bedf31fa6374026f900c481ca752a8
TodoList <juwonjeon852@gmail.com>
2024-03-08 17:45:51 +0900

    Calculator Class를 생성해서 연산하는 메소드를 추가

---
+++ app/src/main/java/com/example/test1/Main.kt
@@ -0,0 +1,15 @@
+package com.example.test1
+
+
+fun main() {
+
+    println("계산기 시작!!!")
+    println("------------")
+    println("계산할 연산식을 입력해주세요. 종료는 -1을 눌러주세요")
+
+
+    // Calculator 클래스 실행
+    Calculator().Operation()
+
+
+}
